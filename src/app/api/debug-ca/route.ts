9cd3998e82d9cc542bf15bc855df19931ad054ce
Cristiano Silva <cristiano.godoi@gotmail.com>
2026-02-22 12:16:37 -0300

    feat: Automatic deploy from agent script

--- src/app/api/debug-ca/route.ts
+++ src/app/api/debug-ca/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: Request) {
         const accessToken = await getValidAccessToken();
 
         async function runScan() {
-            let res = await fetch(`https://api-v2.contaazul.com/v1/financeiro/contas-a-pagar?data_vencimento_de=2026-01-01&data_vencimento_ate=2026-01-31`, { headers: { 'Authorization': `Bearer ${accessToken}` } });
+            let res = await fetch(`https://api-v2.contaazul.com/v1/financeiro/eventos-financeiros/934386fc-baff-45a9-9fe3-978643329f27`, { headers: { 'Authorization': `Bearer ${accessToken}` } });
             if (res.ok) {
                 return await res.json();
             } else {
@@ -16,11 +16,21 @@ export async function GET(request: Request) {
             }
         }
 
-        const payables = await runScan();
+        async function runScan2() {
+            let res = await fetch(`https://api-v2.contaazul.com/v1/financeiro/eventos-financeiros/578d5465-b076-448e-8152-46aaa7190352`, { headers: { 'Authorization': `Bearer ${accessToken}` } });
+            if (res.ok) {
+                return await res.json();
+            } else {
+                return { status: res.status, error: await res.text() };
+            }
+        }
+
+        const detail1 = await runScan();
+        const detail2 = await runScan2();
 
         return NextResponse.json({
             success: true,
-            payables
+            detail1, detail2
         });
     } catch (e: any) {
         return NextResponse.json({ success: false, error: e.message }, { status: 500 });

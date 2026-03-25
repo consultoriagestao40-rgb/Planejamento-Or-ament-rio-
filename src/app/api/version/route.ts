ebbca0d97e6c8a495f29e78e34ae4b0939ce8282
Cristiano Silva <cristiano.godoi@gotmail.com>
2026-03-24 21:06:19 -0300

    v62.5: Atualização de versão com rota de manutenção

--- src/app/api/version/route.ts
+++ src/app/api/version/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
     return NextResponse.json({ 
-        version: 'v62.4',
-        lastUpdate: "2026-03-24 21:05 (Correção do Cálculo de Tributos no Orçamento)"
+        version: 'v62.5',
+        lastUpdate: "2026-03-24 21:10 (Correção e Rota de Manutenção para Tributos Retroativos)"
     });
 }
